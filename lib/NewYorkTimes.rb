6c9ffb5f0d4ae2e94446b0a2cbe93d883afd4bfe
Alex Drennen <drennen42@gmail.com>
2014-11-11 08:44:44 -0600

    Testing scheduler on heroku

--- lib/NewYorkTimes.rb
+++ lib/NewYorkTimes.rb
@@ -2,10 +2,12 @@
 require "./../GetKeywords"
 require "./../Requests_and_Responses"
 require 'awesome_print'
+require 'Date'
+
 
 class NewYorkTimes
   NYT_BASE_URL ="http://api.nytimes.com/svc/mostpopular/v2"
-  NYT_APP_KEY = "25c2511fffb22160760720222857b846:6:70154133"
+  NYT_APP_KEY = "295f07d2db55fce19a6bdd330412d2ff:0:70154133"
   NY_BASE_SEARCH_URL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?"
   attr_accessor :all_articles, :initial_articles
   include GetKeywords
@@ -40,12 +42,16 @@ class NewYorkTimes
 
   def search(keyword)
     timespan = Date.today.prev_day.strftime.gsub(/-/, "")
-    response = JSON.parse(get_request(NY_BASE_SEARCH_URL + "q=" + keyword.split(" ").join("+") + "&begin_date=" + timespan + "&api-key=" + NYT_APP_KEY)["response"])
+    # p timespan
+    url = NY_BASE_SEARCH_URL + "q=" + keyword.split(" ").join("+") + "&begin_date=" + timespan + "&api-key=" + NYT_APP_KEY
+    # p url
+    response = JSON.parse(get_request(url))["response"]
+    # p response
     response["docs"].each do |item|
       article = { title: item["headline"]["main"],
                   url: item["web_url"],
-                  abstract: = item["abstract"],
-                  source: = item["source"]
+                  abstract: item["abstract"],
+                  source: item["source"]
                 }
       @searched_articles << article
     end
@@ -55,4 +61,6 @@ end
 
 # Driver Code:
 # nyt = NewYorkTimes.new
-# ap nyt.get_initial_articles
+# # ap nyt.get_initial_articles
+# nyt.search("Chicago Bears")
+# ap nyt.all_articles
