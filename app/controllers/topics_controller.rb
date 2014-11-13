e6086b9e10bb9f2207a377afc8aa29c0592df63f
Vince <vlmist315@gmail.com>
2014-11-12 22:09:30 -0600

    before merging shomari

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -16,7 +16,7 @@ class TopicsController < ApplicationController
     @total_pages = (@total_articles / 4.0).ceil
     @maxDay = Day.last().date.to_s
     @minDay = Day.first().date.to_s
-    @dataset = Popularity.popularitiesAsJSON(@topics.first.popularities.first(30))
+    @dataset = Popularity.popularitiesAsJSON(@topics.first.popularities)
 	end
 
   def splash
